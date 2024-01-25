b0572023951ae38c444f83ae21c5aa3375ad8f45
imranahmmed <imranahamed733@gmail.com>
2024-01-26 05:01:55 +0600

    found 1problem

--- server/index.js
+++ server/index.js
@@ -1,14 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
 import allroutes from "./routes/api/index.js";
-// import authRouter from "./routes/api/auth.js"
 
 dotenv.config()
 const app = express()
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 5000
 const corsOptions = {
     origin: true
 };
@@ -21,8 +20,8 @@ app.get("/", (req, res) => {
 // Db Connection
 mongoose.set("strictQuery", false)
 const dbConnection = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URL)
+    try {                      
+        await mongoose.connect("mongodb+srv://imranAhammed:doctorAppoinmentApp@cluster0.n04tyoc.mongodb.net/?retryWrites=true&w=majority")
         console.log("Database Connected")
     } catch (error) {
         console.log("Database Connection Failed")
@@ -34,8 +33,9 @@ const dbConnection = async () => {
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
-app.use(baseUrl, allroutes)
-app.use(baseUrl, (req, res) => res.status(404).json({ message: "No API Found on This Route." }))
+app.use("/api/v1", allroutes)
+app.use("/api/v1", (req, res) => res.status(404).json({ message: "No API Found on This Route." }))
+// app.use('/auth', authRoutes);
 // app.use(router)
 
 app.listen(port, () => {
